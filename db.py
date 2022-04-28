eb13f84cb61974ea7ed9b6f663f46858f25b81cb
Caio Caik Fresneda de Souza <2200595@salas.aulas>
2022-04-28 18:35:59 -0300

    version 0.1

--- db.py
+++ db.py
@@ -1,15 +1,34 @@
 import ast
 from uuid import uuid4
 import pandas as pd
+import os
 
 class Users_db:
     def __init__(self):
         self._db_Loc_ = 'db/users.csv'
         self._data_ = pd.read_csv(self._db_Loc_)
+        self._userModal_ = {
+            name:str,
+            userId:str,
+            city:str,
+            locations:[],
+
+        }
+        
     
     def get_database (self):
         return self._data_.to_dict()
+
+    def create_database(self):
+        keys = []
+        for index in self._userModal_:
+            keys.append(index)
+
+        newTable = pd.DataFrame(columns=keys)
     
+    def __createDatabse__(self):
+        pass;
+
     def set_newUserInDataBase(self, obj:dict):
         obj["userId"] = str(uuid4())
         obj["locations"] = obj["locations"] if obj["locations"] else []
