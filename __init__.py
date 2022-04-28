36cfccaa986a97e416b0a85b8de955b3670fcac5
Caio Caik Fresneda de Souza <2200595@salas.aulas>
2022-04-28 18:27:23 -0300

    add home link

--- __init__.py
+++ __init__.py
@@ -1,6 +1,8 @@
 from app import * 
 from controller.user_controller import * 
+from controller.home_controoler import *
 
 if __name__ == "__main__":
 	app.api.add_resource(Users,"/users")
+	app.api.add_resource(main,"/")
 	app.app.run(port=3333)
